f4c95b7906454b4897de25f004c3ba0967fa83ce
PurTahan <sina.farahabadi@gmail.com>
2023-04-14 18:38:04 +0330

    add btns of main menu

--- Keramat/Forms/Dashboard/MainForm.cs
+++ Keramat/Forms/Dashboard/MainForm.cs
@@ -1,25 +1,79 @@
-﻿using Services.Ui;
+﻿using Services.AppLayer;
+using Services.Ui;
 
 namespace Keramat.Forms.Dashboard {
     public partial class MainForm : Form {
         private readonly SplashForm splashForm;
         private readonly IFontsService fontsService;
+        private readonly IAppSettingsService appSettingsService;
+        private readonly IAppVersionService appVersionService;
 
         public MainForm(
             SplashForm splashForm,
-            IFontsService fontsService
+            IFontsService fontsService,
+            IAppSettingsService appSettingsService,
+            IAppVersionService appVersionService
             ) {
 
             InitializeComponent();
             this.splashForm = splashForm;
             this.fontsService = fontsService;
+            this.appSettingsService = appSettingsService;
+            this.appVersionService = appVersionService;
         }
 
         private void MainForm_Load(object sender, EventArgs e) {
-            this.Hide();
             var result = splashForm.ShowDialog();
-            this.Font = fontsService.Get()?.DefaultFont;
-            this.Show();
+            //this.Font = fontsService.Get()?.DefaultFont;
+
+            load();
+        }
+
+        void load() {
+            this.Text = "اپلیکیشن کرامت";
+            lblTitle.Text = "اپلیکیشن کرامت";
+            lblTitle.Font = fontsService.FontByRatio(1.4);
+            lblFooter.Text = $"نسخه: {appVersionService.VersionString()}";
+            lblFooter.Font = fontsService.FontByRatio(0.8);
+
+            addSideButtons();
+        }
+
+        void addSideButtons() {
+            sideBtnPnl.Controls.Clear();
+            var btnList = new Dictionary<string, string>();
+            btnList.Add("valiNematan", "ولی‌نعمتان");
+            btnList.Add("khayyerin", "خیرین");
+            btnList.Add("faaliat", "فعالیت");
+            btnList.Add("gozaresh", "گزارش");
+            btnList.Add("taqvim", "تقویم");
+            btnList.Add("tanzimat", "تنظیمات");
+
+            foreach (var item in btnList) {
+                var ctl = new Button {
+                    Padding = new Padding(8),
+                    Margin = new Padding(0, 0, 0, 10),
+                    Name = item.Key,
+                    Text = item.Value,
+                    Font = fontsService.Get()?.DefaultFont,
+                    Size = new Size(200, 60)
+                };
+                sideBtnPnl.Controls.Add(ctl);
+
+                ctl.Click += sideBtnClick;
+            }
+
+        }
+
+        private void sideBtnClick(object? sender, EventArgs e) {
+            var btn = sender as Button;
+
+            switch (btn?.Name) {
+                case "":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
