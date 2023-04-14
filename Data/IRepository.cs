682da22f277115fd04a64a65d100e6a37946bbc7
PurTahan <sina.farahabadi@gmail.com>
2023-04-14 14:06:15 +0330

    create cancellation token optional

--- Data/IRepository.cs
+++ Data/IRepository.cs
@@ -10,25 +10,25 @@ namespace Data {
 
 
         void Add(TEntity entity, bool saveNow = true);
-        Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken = default, bool saveNow = true);
         void AddRange(IEnumerable<TEntity> entities, bool saveNow = true);
-        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool saveNow = true);
         void Attach(TEntity entity);
         void Delete(TEntity entity, bool saveNow = true);
-        Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+        Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default, bool saveNow = true);
         void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true);
-        Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool saveNow = true);
         void Detach(TEntity entity);
         void LoadCollection<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty) where TProperty : class;
-        Task LoadCollectionAsync<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty, CancellationToken cancellationToken) where TProperty : class;
+        Task LoadCollectionAsync<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty, CancellationToken cancellationToken = default) where TProperty : class;
         void LoadReference<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty) where TProperty : class;
-        Task LoadReferenceAsync<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty, CancellationToken cancellationToken) where TProperty : class;
+        Task LoadReferenceAsync<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty, CancellationToken cancellationToken = default) where TProperty : class;
         void Update(TEntity entity, bool saveNow = true);
-        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default, bool saveNow = true);
         void UpdateRange(IEnumerable<TEntity> entities, bool saveNow = true);
-        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool saveNow = true);
 
         void SaveChanges();
-        Task SaveChangesAsync(CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
