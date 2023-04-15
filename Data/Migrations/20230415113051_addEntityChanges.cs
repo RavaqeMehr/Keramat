dce23a2b73a584a38583391a80ca079321272389
PurTahan <sina.farahabadi@gmail.com>
2023-04-15 15:01:32 +0330

    add EntityChange entity

---
+++ Data/Migrations/20230415113051_addEntityChanges.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addEntityChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntityChanges",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateY = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateM = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateD = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChangeType = table.Column<int>(type: "INTEGER", nullable: false),
+                    EnitityType = table.Column<int>(type: "INTEGER", nullable: false),
+                    EnitityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Root1Id = table.Column<int>(type: "INTEGER", nullable: true),
+                    Root2Id = table.Column<int>(type: "INTEGER", nullable: true),
+                    Root3Id = table.Column<int>(type: "INTEGER", nullable: true),
+                    Changes = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityChanges", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntityChanges");
+        }
+    }
+}
---
+++ Entities/AppUsingLogs/EntityChanges.cs
@@ -0,0 +1,43 @@
+﻿using Entities.Common;
+
+namespace Entities.AppUsingLogs {
+    public class EntityChanges : BaseEntity<long> {
+        public DateTime Date { get; set; }
+        public int DateY { get; set; }
+        public int DateM { get; set; }
+        public int DateD { get; set; }
+
+        public ChangeType ChangeType { get; set; }
+        public EnitityType EnitityType { get; set; }
+
+        public int EnitityId { get; set; }
+        public int? Root1Id { get; set; }
+        public int? Root2Id { get; set; }
+        public int? Root3Id { get; set; }
+
+        public string? Changes { get; set; }
+    }
+
+    public enum ChangeType {
+        Add = 1,
+        Edit = 2,
+        Delete = 3
+    }
+
+    public enum EnitityType {
+        NotSet = -1,
+
+        Family = 100,
+        FamilyLevel = 101,
+        FamilyNeed = 102,
+        FamilyNeedSubject = 103,
+        Connector = 104,
+
+        FamilyMember = 110,
+        FamilyMemberNeed = 111,
+        FamilyMemberNeedSubject = 112,
+        FamilyMemberRelation = 113,
+        FamilyMemberSpecialDisease = 114,
+        FamilyMemberSpecialDiseaseSubject = 115,
+    }
+}
