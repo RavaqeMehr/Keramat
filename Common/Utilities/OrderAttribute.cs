42a038f803709234fc2ea284043c4ff0fcf6b335
PurTahan <sina.farahabadi@gmail.com>
2023-04-15 18:37:06 +0330

    add order attribute filter

---
+++ Common/Utilities/OrderAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Common.Utilities {
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class OrderAttribute : Attribute {
+        private readonly int order_;
+        public OrderAttribute([CallerLineNumber] int order = 0) {
+            order_ = order;
+        }
+
+        public int Order { get { return order_; } }
+    }
+}
--- Entities/Common/BaseEntity.cs
+++ Entities/Common/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Common.Utilities;
+using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -8,6 +9,7 @@ namespace Entities.Common {
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public abstract class BaseEntity<TKey> : IEntity {
+        [Order(-100)]
         [Display(Name = "کد")]
         public TKey Id { get; set; }
     }
