6f69add75a3a3b8c870c902c800efebeb7b5ee35
PurTahan <sina.farahabadi@gmail.com>
2023-04-15 18:37:43 +0330

    get display name

--- Common/Utilities/Comparision.cs
+++ Common/Utilities/Comparision.cs
@@ -1,14 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 
 namespace Common.Utilities {
 
     public static class Comparision {
         public static List<Variance> Compare<T>(this T val1, T val2) {
             var variances = new List<Variance>();
-            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var properties = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => !x.GetMethod.IsVirtual)
+                .OrderBy(x => x.GetCustomAttributes<OrderAttribute>(false).FirstOrDefault()?.Order ?? 0);
             foreach (var property in properties) {
                 var v = new Variance {
-                    PropName = property.Name,
+                    PropName = property.GetDisplayName() ?? property.Name,
                     valA = property.GetValue(val1),
                     valB = property.GetValue(val2)
                 };
@@ -30,12 +34,13 @@ namespace Common.Utilities {
             return variances;
         }
 
-        public static string? ToString(this List<Variance> variances) {
+        public static string? Print(this List<Variance> variances) {
             string? output = null;
             foreach (var item in variances) {
-                output += $"\n'{item.GetDisplayNameOrObjectName()}': [{item.valA}] => [{item.valB}]";
+                output += $"\n[{item.PropName.GetDisplayNameOrValue()}]: {{{item.valA}}} => {{{item.valB}}}";
             }
 
+            Debug.WriteLine(output);
             return output is null ? null : output.Substring(1);
         }
     }
--- Common/Utilities/DataAnotationEtentions.cs
+++ Common/Utilities/DataAnotationEtentions.cs
@@ -1,19 +1,62 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Common.Utilities {
     public static class DataAnotationEtentions {
-        public static string GetDisplayNameOrObjectName(
-            this object? obj,
-            [CallerArgumentExpression(nameof(obj))] string objName = null
-            ) {
-            return objName;
-            //var type = obj.GetType();
-            //return type.Name;
-            //var display = type.GetCustomAttributes<DisplayAttribute>(false).FirstOrDefault();
-            //return display?.Name ?? objName ?? "[]";
-            //return display?.GetType().GetProperty("Name")?.GetValue(display, null)?.ToString() ?? objName ?? "[]";
-            //return display?.ToJSON() ?? objName ?? "[]";
 
+        public static string? GetDisplayName(this PropertyInfo obj) {
+            var display = obj.GetCustomAttributes<DisplayAttribute>(false).FirstOrDefault();
+            return display?.Name;
+        }
+
+        public static string? GetDisplayName<T>(this T obj) {
+            var type = typeof(T);
+            var display = type.GetCustomAttributes<DisplayAttribute>(false).FirstOrDefault();
+            return display?.Name;
+        }
+
+        public static string GetPropertyDisplayNameOrName<T>(
+            this T obj,
+            Expression<Func<T, object>> func,
+            [CallerArgumentExpression(nameof(obj))] string objName = ""
+            ) {
+            var member = GetPropertyInformation(func.Body);
+            return member.GetCustomAttributes<DisplayAttribute>(false).FirstOrDefault()?.Name ?? objName;
+        }
+
+
+        public static string GetDisplayNameOrObjectName<T>(
+            this T obj,
+            [CallerArgumentExpression(nameof(obj))] string objName = ""
+            ) {
+            var displayName = obj.GetDisplayName();
+            return displayName ?? objName ?? "[]";
+        }
+
+        public static string GetDisplayNameOrValue<T>(this T obj) {
+            var displayName = obj.GetDisplayName();
+            return displayName ?? obj.ToString() ?? "[]";
+        }
+
+
+        public static MemberInfo GetPropertyInformation(Expression propertyExpression) {
+            Debug.Assert(propertyExpression != null, "propertyExpression != null");
+            MemberExpression memberExpr = propertyExpression as MemberExpression;
+            if (memberExpr == null) {
+                UnaryExpression unaryExpr = propertyExpression as UnaryExpression;
+                if (unaryExpr != null && unaryExpr.NodeType == ExpressionType.Convert) {
+                    memberExpr = unaryExpr.Operand as MemberExpression;
+                }
+            }
+
+            if (memberExpr != null && memberExpr.Member.MemberType == MemberTypes.Property) {
+                return memberExpr.Member;
+            }
+
+            return null;
         }
     }
 }
