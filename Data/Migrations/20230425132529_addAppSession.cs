088c5582df0a9fe04947c17a978a3b64742e0dbf
PurTahan <sina.farahabadi@gmail.com>
2023-04-25 16:55:55 +0330

    add app session entity

---
+++ Data/Migrations/20230425132529_addAppSession.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAppSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppSessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StartDateY = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartDateM = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartDateD = table.Column<int>(type: "INTEGER", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDateY = table.Column<int>(type: "INTEGER", nullable: false),
+                    EndDateM = table.Column<int>(type: "INTEGER", nullable: false),
+                    EndDateD = table.Column<int>(type: "INTEGER", nullable: false),
+                    DurationSeconds = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppSessions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppSessions");
+        }
+    }
+}
---
+++ Entities/AppUsingLogs/AppSession.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Common;
+
+namespace Entities.AppUsingLogs {
+    public class AppSession : BaseEntity {
+        public DateTime StartDate { get; set; }
+        public int StartDateY { get; set; }
+        public int StartDateM { get; set; }
+        public int StartDateD { get; set; }
+
+        public DateTime EndDate { get; set; }
+        public int EndDateY { get; set; }
+        public int EndDateM { get; set; }
+        public int EndDateD { get; set; }
+
+        public int DurationSeconds { get; set; }
+    }
+}
