0804e2dd604449b5c79788604eac47149bfa2e4e
PurTahan <sina.farahabadi@gmail.com>
2023-04-29 00:54:11 +0330

    786 action

---
+++ KeramatWeb/Api/V1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Frameworks.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeramatWeb.Api.V1.Controllers {
+    [ApiVersion("1")]
+    public class HomeController : BaseApi {
+        [HttpGet]
+        public int Index() {
+            return 786;
+        }
+    }
+}
