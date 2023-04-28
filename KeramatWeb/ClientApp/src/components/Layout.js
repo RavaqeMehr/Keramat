f973fdb76914815c1b5b5b76faad58233b00ad62
PurTahan <sina.farahabadi@gmail.com>
2023-04-28 06:23:09 +0330

    rtling and add vazir font

--- KeramatWeb/ClientApp/src/components/Layout.js
+++ KeramatWeb/ClientApp/src/components/Layout.js
@@ -3,16 +3,13 @@ import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render() {
-    return (
-      <div>
-        <NavMenu />
-        <Container tag="main">
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+	static displayName = Layout.name;
+	render() {
+		return (
+			<div>
+				<NavMenu />
+				<Container tag='main'>{this.props.children}</Container>
+			</div>
+		);
+	}
 }
