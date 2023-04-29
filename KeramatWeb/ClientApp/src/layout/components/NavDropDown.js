35b414194bfdf892e209ae70b96deadb5d99969c
PurTahan <sina.farahabadi@gmail.com>
2023-04-30 02:30:35 +0330

    nested nav menu

--- KeramatWeb/ClientApp/src/layout/components/NavDropDown.js
+++ KeramatWeb/ClientApp/src/layout/components/NavDropDown.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Dropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap';
 
-const NavDropDown = ({ title, items = [], badge = { color, text } }) => {
-	return null;
+const NavDropDown = ({ title, items = [], isRoot = true }) => {
+	const [open, openSet] = useState(false);
+	return (
+		<Dropdown nav isOpen={open} toggle={() => openSet((old) => !old)} direction={isRoot ? 'down' : 'start'}>
+			<DropdownToggle nav className='text-dark' caret>
+				{title}
+			</DropdownToggle>
+			<DropdownMenu>
+				{items.map((x, i) => (
+					<x.component key={i} {...x} isRoot={false} />
+				))}
+			</DropdownMenu>
+		</Dropdown>
+	);
 };
 
 export default NavDropDown;
