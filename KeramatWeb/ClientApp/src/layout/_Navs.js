a5993e3f20d6423f0d1c10c9195cf24847074f3d
PurTahan <sina.farahabadi@gmail.com>
2023-04-30 02:31:59 +0330

    add vali-nematan nav menu link

--- KeramatWeb/ClientApp/src/layout/_Navs.js
+++ KeramatWeb/ClientApp/src/layout/_Navs.js
@@ -1,10 +1,58 @@
 import NavLink from './components/NavLink';
+import NavDropDown from './components/NavDropDown';
 
 const _Navs = [
 	{
 		component: NavLink,
 		title: 'پیش‌خان',
 		to: '/',
+	},
+	// {
+	// 	component: NavDropDown,
+	// 	title: 'ولی‌نعمتان',
+	// 	items: [
+	// 		{
+	// 			component: NavLink,
+	// 			title: 'الف',
+	// 			to: '/',
+	// 		},
+	// 		{
+	// 			component: NavDropDown,
+	// 			title: 'سطح 2',
+	// 			items: [
+	// 				{
+	// 					component: NavLink,
+	// 					title: 'الف',
+	// 					to: '/',
+	// 				},
+	// 				{
+	// 					component: NavLink,
+	// 					title: 'ب',
+	// 					to: '/',
+	// 				},
+	// 				{
+	// 					component: NavLink,
+	// 					title: 'جیم3',
+	// 					to: '/',
+	// 				},
+	// 			],
+	// 		},
+	// 		{
+	// 			component: NavLink,
+	// 			title: 'ب',
+	// 			to: '/',
+	// 		},
+	// 		{
+	// 			component: NavLink,
+	// 			title: 'جیم3',
+	// 			to: '/',
+	// 		},
+	// 	],
+	// },
+	{
+		component: NavLink,
+		title: 'ولی‌نعمتان',
+		to: '/vali-nemtan',
 	},
 ];
 
