19343d93e4f4bb481f39a25a8fe71ebafee7ad25
PurTahan <sina.farahabadi@gmail.com>
2023-05-01 23:12:25 +0330

    get connectors list service

---
+++ Services/ValiNematan/GetConnectorsListService.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using Data;
+using Entities.ValiNematan;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Services.ValiNematan.Models;
+
+namespace Services.ValiNematan {
+    public interface IGetConnectorsListService : IScopedDependency {
+        Task<List<GetConnectorsListItemDto>> Exe();
+    }
+
+    public class GetConnectorsListService : IGetConnectorsListService {
+        private readonly IRepository<Connector> connectorRepo;
+
+        public GetConnectorsListService(
+            IRepository<Connector> connectorRepo
+            ) {
+            this.connectorRepo = connectorRepo;
+        }
+
+        public async Task<List<GetConnectorsListItemDto>> Exe() {
+            return await connectorRepo.TableNoTracking
+                .OrderBy(x => x.Name)
+                .ProjectToType<GetConnectorsListItemDto>()
+                .ToListAsync();
+        }
+    }
+}
