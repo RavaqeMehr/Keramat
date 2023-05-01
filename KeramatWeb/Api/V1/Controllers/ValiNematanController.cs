6f3a15a7418534c20f4c1a936473180356509dea
PurTahan <sina.farahabadi@gmail.com>
2023-05-01 23:12:59 +0330

    add services in controller

--- KeramatWeb/Api/V1/Controllers/ValiNematanController.cs
+++ KeramatWeb/Api/V1/Controllers/ValiNematanController.cs
@@ -1,4 +1,5 @@
-﻿using Frameworks.Api;
+﻿using Entities.ValiNematan;
+using Frameworks.Api;
 using Microsoft.AspNetCore.Mvc;
 using Services.Common;
 using Services.ValiNematan;
@@ -8,16 +9,40 @@ namespace KeramatWeb.Api.V1.Controllers {
     [ApiVersion("1")]
     public class ValiNematanController : BaseApi {
         private readonly IGetFamiliesListService getFamiliesListService;
+        private readonly IInsertFamilyService insertFamilyService;
+        private readonly IGetFamilyLevelsListService getFamilyLevelsListService;
+        private readonly IGetConnectorsListService getConnectorsListService;
 
         public ValiNematanController(
-            IGetFamiliesListService getFamiliesListService
+            IGetFamiliesListService getFamiliesListService,
+            IInsertFamilyService insertFamilyService,
+            IGetFamilyLevelsListService getFamilyLevelsListService,
+            IGetConnectorsListService getConnectorsListService
             ) {
             this.getFamiliesListService = getFamiliesListService;
+            this.insertFamilyService = insertFamilyService;
+            this.getFamilyLevelsListService = getFamilyLevelsListService;
+            this.getConnectorsListService = getConnectorsListService;
         }
 
         [HttpGet]
         public async Task<WithPagination<GetFamiliesListItemDto>> List([FromQuery] GetFamiliesListQuery query) {
             return await getFamiliesListService.Search(query);
+        }
+
+        [HttpPost]
+        public async Task<int> Add([FromBody] InsertFamilyDto dto) {
+            return await insertFamilyService.Exe(dto);
+        }
+
+        [HttpGet]
+        public async Task<List<FamilyLevel>> FamilyLevels() {
+            return await getFamilyLevelsListService.Exe();
+        }
+
+        [HttpGet]
+        public async Task<List<GetConnectorsListItemDto>> Connectors() {
+            return await getConnectorsListService.Exe();
         }
     }
 }
