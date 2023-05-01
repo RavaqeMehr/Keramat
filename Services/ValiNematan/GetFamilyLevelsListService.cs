c7b2f3459fe5a979b83b3746ef99eaa7c7bae53e
PurTahan <sina.farahabadi@gmail.com>
2023-05-01 23:12:36 +0330

    get family level service

---
+++ Services/ValiNematan/GetFamilyLevelsListService.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using Data;
+using Entities.ValiNematan;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.ValiNematan {
+    public interface IGetFamilyLevelsListService : IScopedDependency {
+        Task<List<FamilyLevel>> Exe();
+    }
+
+    public class GetFamilyLevelsListService : IGetFamilyLevelsListService {
+        private readonly IRepository<FamilyLevel> familyLevelRepo;
+
+        public GetFamilyLevelsListService(
+            IRepository<FamilyLevel> familyLevelRepo
+            ) {
+            this.familyLevelRepo = familyLevelRepo;
+        }
+
+        public async Task<List<FamilyLevel>> Exe() {
+            return await familyLevelRepo.TableNoTracking
+                .OrderBy(x => x.Level)
+                .ToListAsync();
+        }
+    }
+}
