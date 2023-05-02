e3543ba79deda82c40d00bf2db19bc06bb347d15
PurTahan <sina.farahabadi@gmail.com>
2023-05-02 13:40:17 +0330

    add clone method

---
+++ Common/Utilities/ObjectsEctentionMethods.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Common.Utilities {
+    public static class ObjectsEctentionMethods {
+        public static T Clone<T>(this T self) {
+            var serialized = JsonConvert.SerializeObject(self);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+    }
+}
+
+
