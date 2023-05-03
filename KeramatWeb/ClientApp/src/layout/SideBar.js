5812b4257927b35d7432ea6385e56e75062f6930
PurTahan <sina.farahabadi@gmail.com>
2023-05-03 11:02:29 +0330

    auto close sibling sub menus

--- KeramatWeb/ClientApp/src/layout/SideBar.js
+++ KeramatWeb/ClientApp/src/layout/SideBar.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { Menu, MenuItem, Sidebar, SubMenu } from 'react-pro-sidebar';
 import React, { Link } from 'react-router-dom';
 import _Navs from './_Navs';
 
 const SideBar = () => {
+	const [opens, opensSet] = useState({});
+
 	return (
 		<Sidebar rtl>
 			<Menu
@@ -15,7 +18,18 @@ const SideBar = () => {
 						borderRightStyle: 'dashed',
 					},
 				}}>
-				{_Navs.map((x, i) => (x.to ? <MItem key={i} {...x} /> : <MSubMenu key={i} {...x} />))}
+				{_Navs.map((x, i) =>
+					x.to ? (
+						<MItem key={i} {...x} />
+					) : (
+						<MSubMenu
+							key={i}
+							{...x}
+							open={opens[i] ?? false}
+							onOpenChange={(e) => globalOpenHandler(i, e, opens, opensSet)}
+						/>
+					)
+				)}
 			</Menu>
 		</Sidebar>
 	);
@@ -25,8 +39,36 @@ export default SideBar;
 
 const MItem = ({ title, to }) => <MenuItem component={<Link to={to} />}> {title}</MenuItem>;
 
-const MSubMenu = ({ title, items }) => (
-	<SubMenu label={title}>
-		{items.map((x, i) => (x.to ? <MItem key={i} {...x} /> : <MSubMenu key={i} {...x} />))}
-	</SubMenu>
-);
+const MSubMenu = ({ title, items, open = false, onOpenChange }) => {
+	const [opens, opensSet] = useState({});
+
+	return (
+		<SubMenu label={title} onOpenChange={onOpenChange} open={open}>
+			{items.map((x, i) =>
+				x.to ? (
+					<MItem key={i} {...x} />
+				) : (
+					<MSubMenu
+						key={i}
+						{...x}
+						open={opens[i] ?? false}
+						onOpenChange={(e) => globalOpenHandler(i, e, opens, opensSet)}
+					/>
+				)
+			)}
+		</SubMenu>
+	);
+};
+
+const globalOpenHandler = (key, isOpen, opens, opensSet) => {
+	if (isOpen) {
+		opensSet((old) => {
+			let newOpens = {};
+			Object.keys(old).forEach((v) => (newOpens[v] = false));
+			newOpens['' + key] = true;
+			return newOpens;
+		});
+	} else {
+		opensSet((old) => ({ ...old, ['' + key]: false }));
+	}
+};
