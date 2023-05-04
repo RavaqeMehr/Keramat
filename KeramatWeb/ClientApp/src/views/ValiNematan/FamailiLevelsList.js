47e1f3f4be5f4e5123cbd20d65ee10984b20fe1c
PurTahan <sina.farahabadi@gmail.com>
2023-05-04 07:33:17 +0330

    list of family levels

--- KeramatWeb/ClientApp/src/views/ValiNematan/FamailiLevelsList.js
+++ KeramatWeb/ClientApp/src/views/ValiNematan/FamailiLevelsList.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import MyTable from '../../components/table/MyTable';
 
 const FamailiLevelsList = () => {
-	return null;
+	const navigate = useNavigate();
+	const { familyLevels } = useSelector((x) => x.logic);
+
+	return (
+		<>
+			<MyTable
+				title='لیست سطح‌ها'
+				cols={['کد', 'عنوان', 'ملاحظات', 'سطح']}
+				rows={familyLevels}
+				onRowClick={(x) => navigate(`./${x.id}`, { relative: true })}
+			/>
+			<Button color='success' onClick={(x) => navigate(`./0`, { relative: true })}>
+				افزودن
+			</Button>
+		</>
+	);
 };
 
 export default FamailiLevelsList;
