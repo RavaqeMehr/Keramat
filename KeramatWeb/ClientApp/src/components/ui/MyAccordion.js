f60602d21d19c35da7420a8c53a6b7cf6ee61808
PurTahan <sina.farahabadi@gmail.com>
2023-05-04 08:59:35 +0330

    add myAccordion cmp

---
+++ KeramatWeb/ClientApp/src/components/ui/MyAccordion.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from 'reactstrap';
+
+const MyAccordion = ({ headers, defaultOpenIndex = 0, headersClassName, bodiesClassName, children }) => {
+	const [open, openSet] = useState('' + defaultOpenIndex);
+
+	const toggle = (id) => {
+		if (open === id) {
+			openSet();
+		} else {
+			openSet(id);
+		}
+	};
+
+	return (
+		<Accordion open={open} toggle={toggle}>
+			{children.map((x, i) => (
+				<AccordionItem key={i}>
+					<AccordionHeader targetId={'' + i} className={headersClassName}>
+						{headers ? headers[i] ?? i : i}
+					</AccordionHeader>
+					<AccordionBody accordionId={'' + i} className={bodiesClassName}>
+						{x}
+					</AccordionBody>
+				</AccordionItem>
+			))}
+		</Accordion>
+	);
+};
+
+export default MyAccordion;
