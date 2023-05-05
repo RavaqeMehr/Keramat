92149240b51f4a9f9e5f5a27cb0d09f97ce8e895
PurTahan <sina.farahabadi@gmail.com>
2023-05-05 12:41:10 +0330

    family need subjects front

--- KeramatWeb/ClientApp/src/layout/routes.js
+++ KeramatWeb/ClientApp/src/layout/routes.js
@@ -5,6 +5,8 @@ import FamiliesList from '../views/ValiNematan/FamiliesList';
 import FamilyForm from '../views/ValiNematan/FamilyForm';
 import ConnectorsList from './../views/ValiNematan/ConnectorsList';
 import ConnectorForm from './../views/ValiNematan/ConnectorForm';
+import FamilyNeedSubjectsList from './../views/ValiNematan/FamilyNeedSubjectsList';
+import FamilyNeedSubjectForm from './../views/ValiNematan/FamilyNeedSubjectForm';
 
 const routes = [
 	{ path: '/', name: 'کرامت', element: Home, exact: true },
@@ -14,6 +16,13 @@ const routes = [
 	{ path: '/vali-nematan/levels/:id', name: 'فرم سطح خانواده', element: FamailiLevelForm },
 	{ path: '/vali-nematan/connectors', name: 'رابط‌های خانواده', element: ConnectorsList, exact: true },
 	{ path: '/vali-nematan/connectors/:id', name: 'فرم رابط خانواده', element: ConnectorForm },
+	{
+		path: '/vali-nematan/family-needs',
+		name: 'نیازهای خانواده',
+		element: FamilyNeedSubjectsList,
+		exact: true,
+	},
+	{ path: '/vali-nematan/family-needs/:id', name: 'فرم نیاز خانواده', element: FamilyNeedSubjectForm },
 ];
 
 export default routes;
---
+++ KeramatWeb/ClientApp/src/views/ValiNematan/FamilyNeedSubjectsList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import MyTable from '../../components/table/MyTable';
+
+const FamilyNeedSubjectsList = () => {
+	const navigate = useNavigate();
+	const { familyNeedSubjects } = useSelector((x) => x.logic);
+
+	return (
+		<>
+			<MyTable
+				title='لیست سطح‌ها'
+				cols={['کد', 'عنوان', 'ملاحظات']}
+				rows={familyNeedSubjects}
+				rowRenderer={rowRenderer}
+				onRowClick={(x) => navigate(`./${x.id}`, { relative: true })}
+			/>
+			<Button color='success' onClick={(x) => navigate(`./0`, { relative: true })}>
+				افزودن
+			</Button>
+		</>
+	);
+};
+
+export default FamilyNeedSubjectsList;
+
+const rowRenderer = (x) => (
+	<>
+		<th scope='row'>{x.id}</th>
+		<td>{x.title}</td>
+		<td>{x.description}</td>
+	</>
+);
