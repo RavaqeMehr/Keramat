43026a0533eb36d3decbeeb4b74d5b13628148d9
PurTahan <sina.farahabadi@gmail.com>
2023-05-10 15:17:28 +0330

    add other enums

--- KeramatWeb/ClientApp/src/enums.js
+++ KeramatWeb/ClientApp/src/enums.js
@@ -1,24 +1,47 @@
-const enumItem = (key, val, text) => ({ key, val, text });
+const enumItemMaker = (key, val, text) => ({ key, val, text });
 
 export const _LiveStatus = [
-	enumItem('NotSet', -1, 'نامشخص'),
-	enumItem('Live', 0, 'زنده'),
-	enumItem('Coma', 1, 'کما'),
-	enumItem('Dead', 2, 'فوت‌شده'),
-	enumItem('Martyr', 3, 'شهید'),
+	enumItemMaker('NotSet', -1, 'نامشخص'),
+	enumItemMaker('Live', 0, 'زنده'),
+	enumItemMaker('Coma', 1, 'کما'),
+	enumItemMaker('Dead', 2, 'فوت‌شده'),
+	enumItemMaker('Martyr', 3, 'شهید'),
 ];
 
 export const _Gender = [
-	enumItem('NotSet', -1, 'نامشخص'),
-	enumItem('Male', 0, 'مذکر'),
-	enumItem('Female', 1, 'مونث'),
-	enumItem('Intersex', 2, 'دوجنسه'),
+	enumItemMaker('NotSet', -1, 'نامشخص'),
+	enumItemMaker('Male', 0, 'مذکر'),
+	enumItemMaker('Female', 1, 'مونث'),
+	enumItemMaker('Intersex', 2, 'دوجنسه'),
 ];
 
 export const _MaritalStatus = [
-	enumItem('NotSet', -1, 'نامشخص'),
-	enumItem('Single', 0, 'مجرد'),
-	enumItem('Married', 1, 'متاهل'),
-	enumItem('Widow', 2, 'بیوه'),
-	enumItem('Divorced', 3, 'مطلقه'),
+	enumItemMaker('NotSet', -1, 'نامشخص'),
+	enumItemMaker('Single', 0, 'مجرد'),
+	enumItemMaker('Married', 1, 'متاهل'),
+	enumItemMaker('Widow', 2, 'بیوه'),
+	enumItemMaker('Divorced', 3, 'مطلقه'),
+];
+
+export const _ChangeType = [
+	enumItemMaker('Add', 1, 'افزودن'),
+	enumItemMaker('Edit', 2, 'ویرایش'),
+	enumItemMaker('Delete', 3, 'حذف'),
+];
+
+export const _EnitityType = [
+	enumItemMaker('NotSet', -1, 'نامشخص'),
+
+	enumItemMaker('Family', 100, 'خانواده'),
+	enumItemMaker('FamilyLevel', 101, 'سطح خانواده'),
+	enumItemMaker('FamilyNeed', 102, 'نیاز خانواده'),
+	enumItemMaker('FamilyNeedSubject', 103, 'موضوع نیاز خانواده'),
+	enumItemMaker('Connector', 104, 'معرف'),
+
+	enumItemMaker('FamilyMember', 110, 'عضو'),
+	enumItemMaker('FamilyMemberNeed', 111, 'نیاز عضو'),
+	enumItemMaker('FamilyMemberNeedSubject', 112, 'موضوع نیاز عضو'),
+	enumItemMaker('FamilyMemberRelation', 113, 'نسبت خانوادگی'),
+	enumItemMaker('FamilyMemberSpecialDisease', 114, 'بیماری خاص عضو'),
+	enumItemMaker('FamilyMemberSpecialDiseaseSubject', 115, 'موضوع بیماری خاص عضو'),
 ];
