8ad207a85e65305385fdf010c4881849599c18f7
PurTahan <sina.farahabadi@gmail.com>
2023-05-10 15:17:01 +0330

    add time show ability

--- KeramatWeb/ClientApp/src/components/dateTime/MyDateTime.js
+++ KeramatWeb/ClientApp/src/components/dateTime/MyDateTime.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { faDate } from '../../helpers/TimeHelpers';
 import { daysAgo } from './../../helpers/TimeHelpers';
 
-const MyDateTime = ({ dateTime }) => (
+const MyDateTime = ({ dateTime, showTime = false, timeOnNewLine = false, showTimeAgo = true }) => (
 	<>
-		{faDate(dateTime)} <small>[{daysAgo(dateTime)}]</small>
+		{faDate(dateTime, showTime, timeOnNewLine)} {showTimeAgo ? <small>[{daysAgo(dateTime)}]</small> : null}
 	</>
 );
 
--- KeramatWeb/ClientApp/src/helpers/TimeHelpers.js
+++ KeramatWeb/ClientApp/src/helpers/TimeHelpers.js
@@ -1,6 +1,22 @@
 import dayjs from 'dayjs';
 
-export const daysAgo = date => dayjs(date).fromNow();
+export const daysAgo = (date) => dayjs(date).fromNow();
 
-export const faDate = date =>
-  dayjs(date).calendar('jalali').format('YYYY/MM/DD');
+export const faDate = (date, showTime = false, timeOnNewLine = false) => {
+	const _date = dayjs(date).calendar('jalali');
+	if (showTime) {
+		if (timeOnNewLine) {
+			return (
+				<>
+					{_date.format('HH:mm:ss')}
+					<br />
+					<small>{_date.format('YYYY/MM/DD')}</small>
+				</>
+			);
+		} else {
+			return _date.format(showTime ? 'YYYY/MM/DD-HH:mm:ss' : 'YYYY/MM/DD');
+		}
+	} else {
+		return _date.format('YYYY/MM/DD');
+	}
+};
