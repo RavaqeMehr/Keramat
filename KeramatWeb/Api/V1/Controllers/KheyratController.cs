fedfc5bae90a60f7650768532b837d10ec0cb12d
PurTahan <sina.farahabadi@gmail.com>
2023-05-12 17:30:38 +0330

    kheyrs api

--- KeramatWeb/Api/V1/Controllers/KheyratController.cs
+++ KeramatWeb/Api/V1/Controllers/KheyratController.cs
@@ -12,19 +12,35 @@ namespace KeramatWeb.Api.V1.Controllers {
         private readonly IInsertNikooKarService insertNikooKarService;
         private readonly IUpdateNikooKarService updateNikooKarService;
         private readonly IRemoveNikooKarService removeNikooKarService;
+        private readonly IGetKheyrsListService getKheyrsListService;
+        private readonly IGetKheyrByIdService getKheyrByIdService;
+        private readonly IInsertKheyrService insertKheyrService;
+        private readonly IUpdateKheyrService updateKheyrService;
+        private readonly IRemoveKheyrService removeKheyrService;
 
         public KheyratController(
             IGetNikooKarsListService getNikooKarsListService,
             IGetNikooKarByIdService getNikooKarByIdService,
             IInsertNikooKarService insertNikooKarService,
             IUpdateNikooKarService updateNikooKarService,
-            IRemoveNikooKarService removeNikooKarService
+            IRemoveNikooKarService removeNikooKarService,
+        ////////////
+            IGetKheyrsListService getKheyrsListService,
+            IGetKheyrByIdService getKheyrByIdService,
+            IInsertKheyrService insertKheyrService,
+            IUpdateKheyrService updateKheyrService,
+            IRemoveKheyrService removeKheyrService
             ) {
             this.getNikooKarsListService = getNikooKarsListService;
             this.getNikooKarByIdService = getNikooKarByIdService;
             this.insertNikooKarService = insertNikooKarService;
             this.updateNikooKarService = updateNikooKarService;
             this.removeNikooKarService = removeNikooKarService;
+            this.getKheyrsListService = getKheyrsListService;
+            this.getKheyrByIdService = getKheyrByIdService;
+            this.insertKheyrService = insertKheyrService;
+            this.updateKheyrService = updateKheyrService;
+            this.removeKheyrService = removeKheyrService;
         }
 
         [HttpGet]
@@ -50,6 +66,33 @@ namespace KeramatWeb.Api.V1.Controllers {
         [HttpDelete]
         public async Task<bool> RemoveNikooKar([FromQuery] int id) {
             return await removeNikooKarService.Exe(id);
+        }
+
+        ////////////
+
+        [HttpGet]
+        public async Task<WithPagination<GetKheyrsListItemDto>> Kheyrat([FromQuery] GetKheyrsListQuery query) {
+            return await getKheyrsListService.Exe(query);
+        }
+
+        [HttpGet]
+        public async Task<KheyrDto> Kheyr([FromQuery] int id) {
+            return await getKheyrByIdService.Exe(id);
+        }
+
+        [HttpPost]
+        public async Task<int> AddKheyr([FromBody] KheyrInsertDto dto) {
+            return await insertKheyrService.Exe(dto);
+        }
+
+        [HttpPut]
+        public async Task<bool> EditKheyr([FromBody] KheyrUpdateDto dto) {
+            return await updateKheyrService.Exe(dto);
+        }
+
+        [HttpDelete]
+        public async Task<bool> RemoveKheyr([FromQuery] int id) {
+            return await removeKheyrService.Exe(id);
         }
 
     }
