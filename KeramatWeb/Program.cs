74a934924bed1171d9b0e8fcbd1d059239793f19
PurTahan <sina.farahabadi@gmail.com>
2023-05-19 11:32:42 +0330

    config ports

--- KeramatWeb/Program.cs
+++ KeramatWeb/Program.cs
@@ -3,21 +3,24 @@ using Autofac.Extensions.DependencyInjection;
 using Frameworks.Configurations;
 using Frameworks.Middlewares;
 using Frameworks.Swagger;
-using Microsoft.AspNetCore.StaticFiles;
-using Microsoft.Extensions.FileProviders;
 using Services.AppLayer;
+using System.Diagnostics;
 
 IServiceProvider serviceProvider;
-
-var filesPath = @"data\files";
-if (!Directory.Exists(filesPath)) {
-    Directory.CreateDirectory(filesPath);
-}
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.AddServices());
+
+const int port = 1404;
+builder.WebHost
+    .UseKestrel(k => {
+        k.ListenAnyIP(port);
+    })
+    .UseIISIntegration()
+    .UseUrls($"http://0.0.0.0:{port}");
+
 
 builder.Services.AddControllersWithViews();
 
@@ -31,33 +34,12 @@ var app = builder.Build();
 serviceProvider = app.Services;
 
 try {
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment()) {
-        app.UseSwaggerAndUI();
-    }
-    else {
-        // just for local use only
-        app.UseSwaggerAndUI();
-
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-        app.UseHttpsRedirection();
-    }
+    app.UseSwaggerAndUI();
 
     app.IntializeDatabase();
     app.UseCustomExceptionHandler();
-
-    FileExtensionContentTypeProvider contentTypes = new FileExtensionContentTypeProvider();
-    contentTypes.Mappings[".apk"] = "application/vnd.android.package-archive";
-    app.UseStaticFiles(new StaticFileOptions() {
-        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), filesPath)),
-        RequestPath = new PathString("/Files"),
-        ContentTypeProvider = contentTypes
-    });
-
+    app.UseStaticFiles();
     app.UseRouting();
-
 
     app.MapControllerRoute(
         name: "default",
@@ -69,6 +51,7 @@ try {
         tmpScope.ServiceProvider.GetRequiredService<ILoadAppSettingsService>().Exe().ConfigureAwait(false);
     }
 
+    Process.Start(new ProcessStartInfo { UseShellExecute = true, FileName = $"http://localhost:{port}" });
     app.Run();
 }
 catch (Exception e) {
@@ -80,4 +63,4 @@ finally {
 
 Environment.Exit(110);
 
-
+Console.Read();
