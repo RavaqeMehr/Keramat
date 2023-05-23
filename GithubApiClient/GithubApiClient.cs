48c35551b1e44b95f04add29a41ce290297e9b02
PurTahan <sina.farahabadi@gmail.com>
2023-05-23 14:22:17 +0330

    create github client

---
+++ GithubApiClient/GithubApiClient.cs
@@ -0,0 +1,24 @@
+﻿using GithubApiClient.Models;
+
+namespace GithubApiClient {
+
+    public class GithubApiClient {
+        private readonly string owner;
+        private readonly string repo;
+
+        public GithubApiClient(
+            string owner,
+            string repo
+            ) {
+            this.owner = owner;
+            this.repo = repo;
+        }
+
+
+        public async Task<List<Release>?> GetReleases() {
+            var service = new Services.GetReleasesService(owner, repo);
+            return await service.Exe();
+        }
+
+    }
+}
---
+++ GithubApiClient/Services/GetReleasesService.cs
@@ -0,0 +1,30 @@
+﻿using GithubApiClient.Models;
+using RestSharp;
+
+namespace GithubApiClient.Services {
+    public class GetReleasesService {
+        private readonly string owner;
+        private readonly string repo;
+
+        private RestClient client;
+
+        public GetReleasesService(
+            string owner,
+            string repo
+            ) {
+            this.owner = owner;
+            this.repo = repo;
+
+            client = new RestClient();
+        }
+
+        public async Task<List<Release>?> Exe() {
+            var url = $@"https://api.github.com/repos/{owner}/{repo}/releases";
+
+            var req = new RestRequest(url);
+            var res = await client.ExecuteAsync<List<Release>>(req);
+
+            return res.Data;
+        }
+    }
+}
